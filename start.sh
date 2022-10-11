dfaeb6c3b116ba2e80f3db28fe0707dafbccfed5
lalyos <lalyos@yahoo.com>
2022-10-11 12:46:34 +0200

    add /failnext endpoint with links

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-#cat > /var/www/html/index.html <<EOF
+
+echo -n "${FAIL:=0}" > /tmp/fail
+chown nginx:nginx /tmp/fail
 cat > /usr/share/nginx/html/index.html <<EOF
 <html>
 <body bgcolor="$COLOR">
@@ -10,6 +12,16 @@ $BODY
 
 <hr/>
 hostname: $HOSTNAME
+<br>
+<a href="/ready" target="_blank" >readiness</a>
+&nbsp;(Fail next: 
+&nbsp;<a target="hidden" href="/failnext?count=1">1</a>
+&nbsp;<a target="hidden" href="/failnext?count=3">3</a>
+&nbsp;<a target="hidden" href="/failnext?count=5">5</a>
+&nbsp;<a target="hidden" href="/failnext?count=10">10</a>)
+
+<iframe name="hidden" hidden></iframe>
+
 </body>
 </html>
 EOF
